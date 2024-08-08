eef5ed368b18d8c42e4dc9717be97f2bd0f10a27
VictoryChibueze <alhajivicci12@gmail.com>
2024-08-08 20:56:39 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
+import Connect from "./components/Connect.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/blog" element={<h1>blog</h1>} />
         </Routes>
       </BrowserRouter>
+      <Connect />
     </main>
   );
 }
---
+++ src/components/Connect.jsx
@@ -0,0 +1,17 @@
+import "../styles/Connect.css";
+
+function Connect() {
+  return (
+    <div className="overlay">
+      <section className="connect">
+        <span></span>
+        <h4>Connect Wallet</h4>
+
+        <input type="checkbox" name="" id="" />
+        <label htmlFor=""></label>
+      </section>
+    </div>
+  );
+}
+
+export default Connect;
